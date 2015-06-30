356e4bbddf0f108e35b48a874e00555a07d9a878
paulo <paulo@brandwatch.com>
2015-06-30 17:12:57 +0100

    Developing new functions mother foca. =)

--- test/arrays.js
+++ test/arrays.js
@@ -1,20 +1,66 @@
-var TT = require('../titao.js');
+var TT = require("../titao.js");
 var assert = require("assert")
 
-describe('Array', function(){
-  describe('isArray()', function(){
-    it('should return false when is not array', function(){
+describe("Array", function(){
+  describe("isArray()", function(){
+    it("should return false when is not array", function(){
       assert.equal(false, TT.isArray({}));
       assert.equal(false, TT.isArray(1));
       assert.equal(false, TT.isArray("Array"));
       assert.equal(false, TT.isArray(true));
     })
-    it('should return true when is an array', function(){
+    it("should return true when is an array", function(){
       assert.equal(true, TT.isArray([]));
       assert.equal(true, TT.isArray([1]));
       assert.equal(true, TT.isArray([1,2,3]));
       assert.equal(true, TT.isArray([1,undefined,3]));
       assert.equal(true, TT.isArray([1,undefined,"3"]));
+    })
+  })
+  describe("first()", function(){
+    it("should return undefined", function(){
+      assert.equal(undefined, TT.first([]));
+      assert.equal(undefined, TT.first(1));
+      assert.equal(undefined, TT.first({}));
+      assert.equal(undefined, TT.first(null));
+      assert.equal(undefined, TT.first([undefined, 0, 1]));
+    })
+    it("should return the first value of array", function(){
+      assert.equal(3, TT.first([3,2,1]));
+      assert.equal(1, TT.first([1]));
+      assert.equal("T", TT.first("TITAO"));
+      assert.equal(undefined, TT.first([undefined, 0, 1]));
+      assert.equal(null, TT.first([null, 0, 1]));
+      assert.deepEqual(undefined, TT.first([, 0, 1]));
+    })
+  })
+  describe("initial()", function(){
+    it("should return an array", function(){
+      assert.deepEqual([1,2], TT.initial([1,2,3], 1));
+      assert.deepEqual([1,2], TT.initial([1,2,3]));
+      assert.deepEqual([1], TT.initial([1,2,3,4,5], 4));
+      assert.deepEqual([1,2,3], TT.initial([1,2,3], 0));
+      assert.deepEqual([1,2,3], TT.initial([1,2,3], -1));
+      assert.deepEqual([], TT.initial({}));
+    })
+  })
+  describe("head()", function(){
+    it("should return an array", function(){
+      assert.deepEqual([1, 2], TT.head([1,2,3]));
+      assert.deepEqual([], TT.head([1]));
+      assert.deepEqual([], TT.head({}));
+      assert.deepEqual([], TT.head([]));
+      assert.deepEqual(TT.initial([1,2,3]), TT.head([1,2,3]));
+      assert.deepEqual(TT.initial([1, 2], 1), TT.head([1,3]));
+    })
+  })
+  describe("rest()", function(){
+    it("should return an array", function(){
+      assert.deepEqual([2, 3], TT.rest([1,2,3]));
+      assert.deepEqual(TT.head([2, 3, 1]), TT.rest([1,2,3]));
+      assert.deepEqual(TT.initial([1, 2, 3], 0), TT.rest([1,2,3], 0));
+      assert.deepEqual([], TT.rest([1,2,3], 3));
+      assert.deepEqual([], TT.rest([1,2,3], 4));
     })
   })
 })
--- titao.js
+++ titao.js
@@ -3,9 +3,35 @@ function TiTao() {
     this.version = '0.0.1';
 }
 
+//Array Functions
+
 TiTao.prototype.isArray = Array.isArray || function (obj) {
     return Object.prototype.toString.call(obj) === '[object Array]';
 };
+
+TiTao.prototype.first = function (array) {
+     if (array == null) return void 0;
+     return array[0];
+};
+
+TiTao.prototype.initial = function (array, n) {
+    return Array.prototype.slice.call(array, 0, array.length - (n == null? 1: n));
+};
+
+TiTao.prototype.head = function (array) {
+    return this.initial(array, 1);
+};
+
+TiTao.prototype.rest = function (array, n) {
+    return Array.prototype.slice.call(array, n == null? 1 : n);
+};
+
+TiTao.prototype.tail = function (array) {
+    return Array.prototype.slice.call(array, 1);
+};
+
+
+//Misc. Functions
 
 TiTao.prototype.getVersion = function () {
     return this.version;
